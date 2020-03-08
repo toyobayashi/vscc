4c0fc001f29d4c32a28d41f97fad2dc5bc1e0c79
toyobayashi <lifenglin314@outlook.com>
2020-03-08 13:00:48 +0800

    28

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const libs = []
 
 if (pkg.dependencies) {
   for (const key in pkg.dependencies) {
-    const dir = path.dirname(require.resolve(key))
+    const dir = findProjectRoot(require.resolve(key))
     if (fs.existsSync(path.join(dir, 'CMakeLists.txt')) || fs.existsSync(path.join(dir, 'CMakelists.txt'))) {
       paths.push(path.relative(__dirname, dir).replace(/\\/g, '/'))
       libs.push(path.basename(key))
@@ -20,4 +20,18 @@ if (process.argv.slice(2)[0] === 'lib') {
   libs.forEach(lib => console.log(lib))
 } else {
   paths.forEach(p => console.log(p))
+}
+
+function findProjectRoot (start) {
+  let current = start ? path.resolve(start) : process.cwd()
+  let previous = ''
+  do {
+    const target = path.join(current, 'package.json')
+    if (fs.existsSync(target) && fs.statSync(target).isFile()) {
+      return current
+    }
+    previous = current
+    current = path.dirname(current)
+  } while (current !== previous)
+  return ''
 }
