df628900fc30bd2c3c25ec6f93d61e5b539cf3aa
toyobayashi <lifenglin314@outlook.com>
2020-03-08 11:51:37 +0800

    23

---
+++ include/vscc/vscc.h
@@ -0,0 +1,31 @@
+#ifndef __VSCC_H__
+#define __VSCC_H__
+
+#ifdef __cplusplus
+  #define EXTERN_C_START extern "C" {
+  #define EXTERN_C_END }
+#else
+  #define EXTERN_C_START
+  #define EXTERN_C_END
+#endif
+
+#ifdef _WIN32
+  #ifdef BUILD_DLL_vscc
+  #define VSCC_API __declspec(dllexport)
+  #else
+  // #define VSCC_API __declspec(dllimport)
+  #define VSCC_API
+  #endif
+#else
+  #ifdef BUILD_DLL_vscc
+  #define VSCC_API __attribute__((visibility("default")))
+  #else
+  #define VSCC_API
+  #endif
+#endif
+
+EXTERN_C_START
+VSCC_API int add(int, int);
+EXTERN_C_END
+
+#endif
---
+++ src/lib/vscc.cpp
@@ -0,0 +1,5 @@
+#include "vscc/vscc.h"
+
+int add(int a, int b) {
+  return a + b;
+}
